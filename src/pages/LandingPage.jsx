46d0fdc06ef14ce7a5ed8fc2ff01f41a7ee0b36a
RezalMaksmi <rezalnurscc@gmail.com>
2024-05-06 23:05:24 +0700

    add card tim landingPage

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -6,8 +6,54 @@ import layanan2 from "../assets/layanan2.jpg";
 import layanan1 from "../assets/layanan1.jpg";
 import layanan3 from "../assets/layanan3.jpg";
 import layanan4 from "../assets/layanan4.jpg";
+
+import aura from "../assets/tim/aura.jpg";
+import rezal from "../assets/tim/rezal.jpg";
+import hasna from "../assets/tim/hasna.jpg";
+import ilham from "../assets/tim/ilham.jpg";
+import ino from "../assets/tim/ino.jpg";
+import sipa from "../assets/tim/sipa.jpg";
+
 import bgHero from "../assets/bg-hero.jpg";
 import logoPutih from "../assets/logo/logoputih.svg";
+
+import "swiper/swiper.min.css";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, Pagination, Navigation } from "swiper";
+
+const dataProfile = [
+  {
+    image: aura,
+    name: "Lintang Aura S",
+    role: "Hustler",
+  },
+  {
+    image: hasna,
+    name: "Raidah Hasna M",
+    role: "Hipster",
+  },
+  {
+    image: sipa,
+    name: "Assyifa Latifah T",
+    role: "Hipster",
+  },
+  {
+    image: rezal,
+    name: "Rezal Nur S",
+    role: "Hacker",
+  },
+  {
+    image: ilham,
+    name: "Abdullah Ilham",
+    role: "Hacker",
+  },
+  {
+    image: ino,
+    name: "Inosensius G. A",
+    role: "Hacker",
+  },
+];
+
 const LandingPage = () => {
   return (
     <div className=" ">
@@ -52,7 +98,7 @@ const LandingPage = () => {
         </div>
 
         {/* {  fitur Layanan  } */}
-        <div className="h-auto px-20 py-36 gap-14 container mx-auto flex flex-col justify-center items-center">
+        <div className="h-auto md:px-20 md:pt-36 px-10 py-12 gap-14 container mx-auto flex flex-col justify-center items-center">
           <h1 className="text-5xl font-semibold">Layanan</h1>
 
           <div className="grid grid-cols-4 gap-6">
@@ -160,6 +206,60 @@ const LandingPage = () => {
               </div>
             </div>
           </div>
+        </div>
+      </div>
+      <div className="h-auto  px-20 pb-36 gap-16 container mx-auto flex flex-col justify-center items-center">
+        <h1 className="text-5xl font-semibold">Tim Kami</h1>
+        <div className=" w-full items-center px-3">
+          <Swiper
+            loop={true}
+            autoplay={{
+              delay: 2500,
+              disableOnInteraction: false,
+            }}
+            pagination={{
+              clickable: true,
+            }}
+            navigation={true}
+            modules={[Autoplay, Pagination, Navigation]}
+            slidesPerView={4}
+            spaceBetween={50}
+            breakpoints={{
+              640: {
+                slidesPerView: 2,
+                spaceBetween: 10,
+              },
+              768: {
+                slidesPerView: 3,
+                spaceBetween: 10,
+              },
+              1024: {
+                slidesPerView: 5,
+                spaceBetween: 40,
+              },
+              1280: {
+                slidesPerView: 5,
+                spaceBetween: 40,
+              },
+            }}
+            onSlideChange={() => console.log("slide change")}
+            onSwiper={(swiper) => console.log(swiper)}
+            className="mySwiper "
+          >
+            {dataProfile.map((item, i) => {
+              return (
+                <SwiperSlide key={i}>
+                  <div className="bg-slate-400 overflow-hidden text-[#fff] flex justify-center w-[230px] h-[300px] relative rounded-3xl">
+                    <img src={item.image} alt="" className="object-cover" />
+                    <div className="absolute bottom-3 text-center">
+                      <h5 className="text-2xl font-semibold">{item.name}</h5>
+                      <span className="text-xl">{item.role}</span>
+                    </div>
+                  </div>
+                </SwiperSlide>
+              );
+            })}
+          </Swiper>
         </div>
       </div>
     </div>
